f832f104d2575674d1efc89436def1a1981cd7fd
Marcus Mouyat <marcus@mouyat.com>
2019-09-03 11:44:51 +1000

    Added files to repository

---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Texture2D mapTexture;
+    public PixelToObject[] pixelColourMappings;
+    private Color pixelColour;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+    void GenerateLevel()
+    {
+        for(int x = 0; x < mapTexture.width; x++)
+        {
+            for (int y = 0; y < mapTexture.height; y++)
+            {
+                GenerateObject(x, y);
+            }
+        }
+    }
+
+    void GenerateObject(int x, int y)
+    {
+        pixelColour = mapTexture.GetPixel(x, y);
+
+        if (pixelColour.a != 0)
+        {
+            foreach(PixelToObject pcm in pixelColourMappings) // pixelColourMapping
+            {
+                if (pcm.pixelColour.Equals(pixelColour))
+                {
+                    Vector2 position = new Vector2(x, y);
+                    Instantiate(pcm.prefab, position, Quaternion.identity, transform);
+                }
+            }
+        }
+        /*
+        if (pixelColour.a == 0)
+        {
+            return;
+        }
+        else
+        {
+            // Do terrain generation stuff
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/PixelToObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class PixelToObject
+{
+    public Color pixelColour;
+    public GameObject prefab;
+}
